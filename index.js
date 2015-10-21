3182a6925e761e09022578fdf409034f10d7dbfa
AlejandroSilva <pm5k.sk@gmail.com>
2015-10-21 02:15:00 -0300

    listo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require('http');
 http.createServer(function (req, res) {
     console.log('peticion GET /');
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('push to deploy funcionando la raja!!  directo a la app');
+    res.end('restart desde un push');
 }).listen(8080);
 console.log('Server running at http://APP_PRIVATE_IP_ADDRESS:8080/');
 
