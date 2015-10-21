2a816a7291e6e57dac1e7c3b6c4a4fb142cb8cc7
AlejandroSilva <pm5k.sk@gmail.com>
2015-10-21 01:46:16 -0300

    listo

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
+    console.log('peticion GET /');
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('push to deploy funcionando la raja!!   ');
 }).listen(8080);
