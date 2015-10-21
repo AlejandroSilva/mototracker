41b5548ee7f87b64ae57d68458d8abf91d1ab01c
AlejandroSilva <pm5k.sk@gmail.com>
2015-10-21 01:44:20 -0300

    hello

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 var http = require('http');
 http.createServer(function (req, res) {
-    res.json({
-        saludo: 'hola mundo!!'
-    })
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('saludos!!');
 }).listen(8080);
 console.log('Server running at http://APP_PRIVATE_IP_ADDRESS:8080/');
 
