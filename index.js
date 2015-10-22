6fd2642c82d28dec721ecdb2fbefb8dbb450f53c
AlejandroSilva <pm5k.sk@gmail.com>
2015-10-22 01:03:02 -0300

    puerto 80

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ http.createServer(function (req, res) {
     console.log('peticion GET /');
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('a dormir!');
-}).listen(80, '0.0.0.0');
+}).listen(80);
 console.log('Server running at http://APP_PRIVATE_IP_ADDRESS:8080/');
 
