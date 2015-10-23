eb2400adc9ecbc5fdc96418500e3d8a84dbe22bf
AlejandroSilva <pm5k.sk@gmail.com>
2015-10-23 02:03:08 -0300

    react+redux+router

--- index.js
+++
@@ -1,9 +0,0 @@
-var port = 80
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log('peticion GET /');
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('a dormir!');
-}).listen(port);
-console.log('Server running at http://APP_PRIVATE_IP_ADDRESS:'+port);
-
